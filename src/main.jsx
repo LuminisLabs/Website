26d24da607aa9f5568185837bf81f7c45fd017be
buttowsk <vinicius_nunes0104@hotmail.com>
2023-05-12 04:29:14 -0300

    páginas e esqueleto

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import { GlobalStyle } from './globalStyles/styles.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle/>
       <App/>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
