cc2b623099393e4916892928d98f6014f5c9a7af
buttowsk <vinicius_nunes0104@hotmail.com>
2023-06-01 04:19:41 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './routes/App.jsx';
 import { GlobalStyle } from './globalStyles/styles.js';
-import { HashRouter } from 'react-router-dom';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <GlobalStyle/>
       <App/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
